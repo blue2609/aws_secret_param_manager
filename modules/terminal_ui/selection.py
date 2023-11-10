d030ab44f3bf7949622e4d5636a834d2331017ca
Stanley Setiawan <stanley.setiawan@beopen.com>
2023-11-10 16:38:39 +1100

    CMD-409: Added 'SsmParamNameComponent' and 'SsmParamName' classes
    These classes are going to be used to automatically format/parse strings
    into the correct AWS Parameter name format

--- modules/terminal_ui/selection.py
+++ modules/terminal_ui/selection.py
@@ -1,6 +1,8 @@
 from .constants import SERVICE_PROJECT_NAME
+from modules.aws.ssm.param_name import SsmParamName, SsmParamNameComponent
 
 class SelectionRecorder:
+
     env_name: str = ""
     proj_name: str = ""
     param_name: str = ""
@@ -16,7 +18,14 @@ class SelectionRecorder:
         cls.service_name = next(
             (service_name for service_name, project_name in SERVICE_PROJECT_NAME if project_name == cls.proj_name), ""
         )
-        return f"/{cls.env_name}/{cls.service_name}/{cls.param_name}"
+        # return f"/{cls.env_name}/{cls.service_name}/{cls.param_name}"
+        return SsmParamName(
+            [
+                SsmParamNameComponent(cls.env_name),
+                SsmParamNameComponent(cls.service_name),
+                SsmParamNameComponent(cls.param_name)
+            ]
+        )
 
 class SetparamSelectionRecorder(SelectionRecorder):
 
